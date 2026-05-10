9927ed7f104e186d675585dacd0221ced7d72827
Tres29 <evangelistaharry68@gmail.com>
2026-05-10 21:37:48 +0800

    m

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@
 
 "use client";
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+
 export default function Home() {
   return (
     <div className="flex flex-col w-screen">
@@ -21,47 +24,59 @@ export default function Home() {
           <button className="hover:bg-gray-700 transition duration-300 rounded-full active:scale-95">Contact</button>
         </div>
       </nav>
-      <div className="hero-section w-screen">
-        <div
-          className="flex flex-col gap-8 absolute left-24 p-4 w-128 h-fit text-white"
-        >
-          <p className="text-9xl">
-            Create Every Moment
-          </p>
-          <button
-            className="px-8 py-4 w-48 border border-white rounded-full hover:bg-gray-700 transition duration-300 active:scale-95"
-          >
-            Book Now
-          </button>
-        </div>
-      </div>
-      <div className="about flex justify-between items-center gap-2 p-24 h-fit bg-white group">
-        <div className="w-1/2">
-          <img 
-            className="transition duration-300 group-hover:scale-110"
-            src="/images/camera-shot.png"
-          />
-        </div>
-        <div className="flex flex-col items-center gap-8 w-1/2">
-          <h2 className="text-4xl font-bold">ABOUT US</h2>
-          <p>
-            We believe that every second holds a story worth keeping. What started as a simple passion for the lens has evolved into a dedicated mission: to freeze time for the moments that matter most.
-          </p>
-          <p>
-            From the quiet, candid smiles to the grandest celebrations of life, our goal is to capture the raw emotion and beauty of your journey. We don't just take pictures; we preserve legacies, one frame at a time.
-          </p>
-        </div>
-      </div>
-      <div className="services flex flex-col items-center h-120 ">
-        <h2 className="text-4xl font-bold">Services</h2>
-        <div className="cards ">
-          <div>Harry - Update 2</div>
-        </div>
-      </div>
-      <div className="gallery">
-        gallery ni ron ito
-      </div>
-      <div className="contact">Contact us</div>
+      <Swiper>
+        <SwiperSlide>
+          <div className="hero-section w-screen">
+            <div
+              className="flex flex-col gap-8 absolute left-24 p-4 w-128 h-fit text-white"
+            >
+              <p className="text-9xl">
+                Create Every Moment
+              </p>
+              <button
+                className="px-8 py-4 w-48 border border-white rounded-full hover:bg-gray-700 transition duration-300 active:scale-95"
+              >
+                Book Now
+              </button>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="about flex justify-between items-center gap-2 p-24 h-fit bg-white group">
+            <div className="w-1/2">
+              <img 
+                className="transition duration-300 group-hover:scale-110"
+                src="/images/camera-shot.png"
+              />
+            </div>
+            <div className="flex flex-col items-center gap-8 w-1/2">
+              <h2 className="text-4xl font-bold">ABOUT US</h2>
+              <p>
+                We believe that every second holds a story worth keeping. What started as a simple passion for the lens has evolved into a dedicated mission: to freeze time for the moments that matter most.
+              </p>
+              <p>
+                From the quiet, candid smiles to the grandest celebrations of life, our goal is to capture the raw emotion and beauty of your journey. We don't just take pictures; we preserve legacies, one frame at a time.
+              </p>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="services flex flex-col items-center h-120 ">
+            <h2 className="text-4xl font-bold">Services</h2>
+            <div className="cards ">
+              <div>Harry - Update 2</div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="gallery">
+            gallery ni ron ito
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="contact">Contact us</div>
+        </SwiperSlide>
+      </Swiper>
       <footer>
         Copyright by Frontdesk Team 2026
       </footer>
