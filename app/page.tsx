d0815c72dfe5eb5c41dd2b1a051dc7eef34f5035
Tres29 <evangelistaharry68@gmail.com>
2026-05-10 20:05:04 +0800

    app:ongoing services update

--- app/page.tsx
+++ app/page.tsx
@@ -51,7 +51,7 @@ export default function Home() {
       <div className="services flex flex-col items-center h-120 ">
         <h2 className="text-4xl font-bold">Services</h2>
         <div className="cards ">
-          <div></div>
+          <div>Harry</div>
         </div>
       </div>
       <div className="gallery">
