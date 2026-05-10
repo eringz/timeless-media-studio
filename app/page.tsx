977ae7a0b9ccfc2d26a91c47ada8d4a0f39e37cf
ASTA <ron.garcia.santos@gmail.com>
2026-05-10 20:23:00 +0800

    app: ongoing testing for collaboration

--- app/page.tsx
+++ app/page.tsx
@@ -55,7 +55,7 @@ export default function Home() {
         </div>
       </div>
       <div className="gallery">
-        gallery ni ron ito
+        gallery ni ron ito ng matapos ma
       </div>
       <div className="contact">Contact us</div>
       <footer>
