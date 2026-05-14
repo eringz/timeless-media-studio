53653f506d61baff09bf61e17a561bb424adab2f
ASTA <ron.garcia.santos@gmail.com>
2026-05-14 15:10:04 +0800

    sections: minimal adjustments

--- sections/services/Services.tsx
+++ sections/services/Services.tsx
@@ -3,6 +3,8 @@
 import SectionHeading from "@/components/ui/SectionHeading";
 import EventService from "@/components/ui/EventService";
 
+
+
 const Services = () => {
     return (
         <div
@@ -10,19 +12,6 @@ const Services = () => {
             className="services flex flex-col gap-6 sm:gap-8 p-4 sm:p-8 md:p-24 bg-gray-50 w-full h-full"
         >
             <SectionHeading title="Services" />
-
-            {/* Marquee Text */}
-            <div className="w-full overflow-hidden bg-black py-4 rounded-xl">
-                <div className="marquee flex whitespace-nowrap">
-                    <span className="text-white text-lg font-semibold px-8">
-                        📸 Photography • 🎉 Birthday Events • 💍 Weddings • 🎈 Debuts • 🎤 Concerts • 👨‍👩‍👧 Family Shoot • 🌅 Prenup • 🏖 Travel Photography •
-                    </span>
-
-                    <span className="text-white text-lg font-semibold px-8">
-                        📸 Photography • 🎉 Birthday Events • 💍 Weddings • 🎈 Debuts • 🎤 Concerts • 👨‍👩‍👧 Family Shoot • 🌅 Prenup • 🏖 Travel Photography •
-                    </span>
-                </div>
-            </div>
 
             <div className="flex flex-col gap-12 lg:px-48 w-full h-full overflow-x-auto">
                 <div className="flex flex-col lg:flex-row gap-12">
@@ -40,6 +29,22 @@ const Services = () => {
                     <EventService name="corporate" event="EDITING" />
                 </div>
             </div>
+
+
+            {/* Marquee Text */}
+            <div className="w-full overflow-hidden bg-black py-4 rounded">
+                <div className="marquee flex whitespace-nowrap">
+                    <span className="text-white text-lg font-semibold px-8">
+                        📸 Photography • 🎉 Birthday Events • 💍 Weddings • 🎈 Debuts • 🎤 Concerts • 👨‍👩‍👧 Family Shoot • 🌅 Prenup • 🏖 Travel Photography •
+                    </span>
+
+                    <span className="text-white text-lg font-semibold px-8">
+                        Photography • 🎉 Birthday Events • 💍 Weddings • 🎈 Debuts • 🎤 Concerts • 👨‍👩‍👧 Family Shoot • 🌅 Prenup • 🏖 Travel Photography •
+                    </span>
+                </div>
+            </div>
+
+            
 
             {/* Animation */}
             <style jsx>{`
