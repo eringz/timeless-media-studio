f0314150d8ce0a6d513e20b5f19913ad49a0383b
ASTA <ron.garcia.santos@gmail.com>
2026-05-20 20:46:41 +0800

    hooks: centralized use effects for mobile and desktop navigation for better impact in performance, time and space

---
+++ hooks/useHeaderVisibility.ts
@@ -0,0 +1,40 @@
+import { useState, useEffect, useRef } from "react";
+
+export const useHeaderVisibility = () => {
+    const [headerVisibility, setHeaderVisibility] = useState(true);
+    const [menuOpen, setMenuOpen] = useState(false);
+    const lastScrollY = useRef(0);
+    const ticking = useRef(false);
+
+    useEffect(() => {
+        const updateHeaderVisibility = () => {
+            const currentScrollY = window.scrollY;
+
+            // console.log(`Current: ${currentScrollY} and Last: ${lastScrollY.current}}`);
+
+            if (currentScrollY > lastScrollY.current && currentScrollY > 100) {
+                setHeaderVisibility(false);
+                setMenuOpen(false);
+            } else {
+                setHeaderVisibility(true);
+            }
+
+            lastScrollY.current = currentScrollY;
+
+            ticking.current = false;
+        }
+
+        const handleScroll = () => {
+            if (!ticking.current) {
+                window.requestAnimationFrame(updateHeaderVisibility);
+                ticking.current = true;
+            }        
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return  () => window.removeEventListener("scroll", handleScroll);
+
+    }, [])
+
+    return { headerVisibility, menuOpen, setMenuOpen };
+}
